b3c31f847bdc1943343447c9d803a044005f3db3
S. Andrew Sheppard <andrew@wq.io>
2016-01-14 16:50:20 -0600

    flake8

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,11 +1,9 @@
 import os
-os.environ['DJANGO_SETTINGS_MODULE'] = "tests.settings"
-
 from django.test.utils import setup_test_environment
-setup_test_environment()
-
 import django
-django.setup()
-
 from django.core.management import call_command
+
+os.environ['DJANGO_SETTINGS_MODULE'] = "tests.settings"
+setup_test_environment()
+django.setup()
 call_command('migrate')
